8711ff5ac62ceb2eb1757dbe9d04d021e5e5c11a
ebubeo504-byte <ebubeo504@gmail.com>
2026-09-14 04:19:55 +0100

    Update webhook.ts

--- src/config/webhook.ts
+++ src/config/webhook.ts
@@ -9,4 +9,4 @@
 // keep working normally, it just won't send real inquiries anywhere.
 // ---------------------------------------------------------------------------
 
-export const MAKE_WEBHOOK_URL = "";
+export const MAKE_WEBHOOK_URL = "https://hook.eu1.make.com/f6bk8i6b9trvye4743xlq8gvwnq9jghs";
